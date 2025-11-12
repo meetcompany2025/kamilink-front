f2cc0082d6140cac5ab91ba2e0038200318fa39d
meetcompany2025 <meetcompany2025@gmail.com>
2025-11-12 09:21:43 +0100

    vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -17,10 +17,21 @@ const nextConfig = {
 
   // ✅ Corrigido: agora temos apenas 1 bloco "experimental"
   // ✅  and habilitamos o  swc via WebAssembly -> compatível com todos sistemas operacionais 
+   // Habilita cache inteligente e rotas dinâmicas no App Router
   experimental: {
     optimizePackageImports: ['lucide-react', '@rad   ix-ui/react-icons'],
     forceSwcTransforms: true, 
+    serverActions: { bodySizeLimit: "2mb" },
+    serverComponentsExternalPackages: ["@prisma/client"],
     // swcLoader: 'wasm',
+  },
+
+    dynamicParams: true,
+
+      // Caso use API externa (como Koyeb), evite bloqueio de CORS no build SSR
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "your-cdn.com"],
   },
 
   compiler: {
